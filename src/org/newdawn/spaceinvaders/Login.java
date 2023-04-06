3fd3350c1014daea4bf31e505d7dc8c64bb0f925
kimsojeong <rtw0202@naver.com>
2023-04-06 16:28:01 +0900

    sojeong_test

---
+++ src/org/newdawn/spaceinvaders/Login.java
@@ -0,0 +1,12 @@
+package org.newdawn.spaceinvaders;
+
+public class Login {
+    public void login(){
+        System.out.println("helloworld!");
+    }
+
+    public static void main(String[] args){
+        Login me = new Login();
+        me.login();
+    }
+}
