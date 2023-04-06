ea087546ddfe38d94748f59c73c5d62a5ef2d664
jinhui-leee <fkdkfk1578@gmail.com>
2023-04-07 01:23:58 +0900

    Framework button modify

--- src/org/newdawn/spaceinvaders/Framework.java
+++ src/org/newdawn/spaceinvaders/Framework.java
@@ -20,6 +20,8 @@ public class Framework extends JPanel implements ActionListener, KeyListener, Mo
 
     JButton[] btn;
 
+    JButton backBtn;
+
     ImageIcon[] btnImage;
 
     BufferedImage[] backgroundImage;
@@ -79,6 +81,9 @@ public class Framework extends JPanel implements ActionListener, KeyListener, Mo
             URL backgroundUrl = this.getClass().getResource("/sprites/background1.png");
             backgroundImage[0] = ImageIO.read(backgroundUrl);
 
+            URL descriptionUrl = this.getClass().getResource("/sprites/description.png");
+            backgroundImage[1] = ImageIO.read(descriptionUrl);
+
         }
         catch (IOException e) {
             Logger.getLogger(Framework.class.getName()).log(Level.SEVERE, null, e);
@@ -86,33 +91,53 @@ public class Framework extends JPanel implements ActionListener, KeyListener, Mo
 
 
 
-        btnImage = new ImageIcon[5];
-        btn = new JButton[5];
+        btnImage = new ImageIcon[6];
+        btn = new JButton[6];
 
 
-        URL[] btnImageUrl = new URL[5];
+        URL[] btnImageUrl = new URL[6];
         btnImageUrl[0] = getClass().getResource("/sprites/btn1.png");
         btnImageUrl[1] = getClass().getResource("/sprites/btn2.png");
         btnImageUrl[2] = getClass().getResource("/sprites/btn3.png");
         btnImageUrl[3] = getClass().getResource("/sprites/btn4.png");
         btnImageUrl[4] = getClass().getResource("/sprites/btn5.png");
+        btnImageUrl[5] = getClass().getResource("/sprites/btn6.png");
 
 
-        for (int i = 0; i < 5; i++) {
+
+        for (int i = 0; i < btn.length; i++) {
             btnImage[i] = new ImageIcon(btnImageUrl[i]);
             btn[i] = new JButton(btnImage[i]);
             btn[i].addActionListener(this);
             btn[i].setBounds(110 + 120 * i, 450, 100, 40);
-            add(btn[i]);
+            this.add(btn[i]);
             btn[i].setVisible(true);
+
         }
 
 
+    }
 
-
-
-
-
+    public void btnManager() {
+        if (gameState == GameState.MAIN_MENU) {
+            btn[5].setVisible(false);
+            setLayout(null);
+            for (int i=0; i<5; i++) {
+                btn[i].setBounds(110 + 120 * i, 450, 100, 40);
+                this.add(btn[i]);
+                btn[i].setVisible(true);
+            }
+        }
+        else
+        {
+            for (int i=0; i<5; i++) btn[i].setVisible(false);
+            setLayout(null);
+            btn[5].setBounds(20, 15, 60, 60);
+            add(btn[5]);
+            btn[5].setVisible(true);
+            btn[5].setContentAreaFilled(false);
+            btn[5].setBorderPainted(false);
+        }
     }
 
     @Override
@@ -172,6 +197,7 @@ public class Framework extends JPanel implements ActionListener, KeyListener, Mo
                 break;
 
             case DESCRIPTION:
+                g2d.drawImage(backgroundImage[1], 0, 0, 800, 600, null);
 
                 break;
 
@@ -194,8 +220,11 @@ public class Framework extends JPanel implements ActionListener, KeyListener, Mo
 
     }
 
+
     @Override
     public void actionPerformed(ActionEvent e) {
+
+
         //게임시작
         if (e.getSource() == btn[0]) {
 
@@ -206,24 +235,28 @@ public class Framework extends JPanel implements ActionListener, KeyListener, Mo
         }
         //게임설명
         else if (e.getSource() == btn[1]) {
-
+            gameState = GameState.DESCRIPTION;
+            btnManager();
         }
         //테마설정
         else if (e.getSource() == btn[2]) {
+            gameState = GameState.THEME;
 
         }
         //캐릭터설정
         else if (e.getSource() == btn[3]) {
-
+            gameState = GameState.CHARACTER;
         }
         //랭킹보기
         else if (e.getSource() == btn[4]) {
-
+            gameState = GameState.RANKING;
         }
         else if (e.getSource() == btn[5]) {
-
+            gameState = GameState.MAIN_MENU;
+            btnManager();
 
         }
+
 
     }
 
