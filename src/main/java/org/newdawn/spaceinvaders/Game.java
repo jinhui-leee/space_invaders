62a340e50c7c78dc24c4573310628980bb6a3836
AISELab <rtw0202@naver.com>
2023-04-07 23:12:46 +0900

    파베 도전 중 혹시 날라갈까봐..

--- src/main/java/org/newdawn/spaceinvaders/Game.java
+++ src/main/java/org/newdawn/spaceinvaders/Game.java
@@ -659,13 +659,14 @@ public class Game extends Canvas implements ActionListener, WindowListener
 	 * @param argv The arguments that are passed into our game
 	 */
 	public static void main(String argv[]) {
-//		Game g = new Game();
-
-		// Start the main game loop, note: this method will not
-		// return until the game has finished running. Hence we are
-		// using the actual main thread to run the game.
-
-//		g.gameLoop();
+////		Game g = new Game();
+//
+//		// Start the main game loop, note: this method will not
+//		// return until the game has finished running. Hence we are
+//		// using the actual main thread to run the game.
+//
+////		g.gameLoop();
 		Window w = new Window();
+//	}
 	}
 }
