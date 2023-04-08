e35caa6f385549c80b1d608b1ef8109c53ca8860
AISELab <rtw0202@naver.com>
2023-04-08 22:53:31 +0900

    realtime database 도전 성공 !!!!!!!!!!!!!!

---
+++ src/main/java/org/newdawn/spaceinvaders/User.java
@@ -0,0 +1,15 @@
+package org.newdawn.spaceinvaders;
+
+public class User {
+    public String email;
+    public String password;
+
+    public User() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+}
