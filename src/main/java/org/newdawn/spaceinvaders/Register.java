6e1df7ce8b149d6128c8da2e80cb39f45d8f99da
AISELab <rtw0202@naver.com>
2023-04-08 20:19:34 +0900

    realtime database 연동 도전..22

--- src/main/java/org/newdawn/spaceinvaders/Register.java
+++ src/main/java/org/newdawn/spaceinvaders/Register.java
@@ -1,26 +1,56 @@
 package org.newdawn.spaceinvaders;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.UserRecord;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
+import com.google.firebase.remoteconfig.User;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class Register extends JPanel implements ActionListener {
-    public static JFrame register;
+
+    JFrame register;
 
     JButton registerbtn;
     JTextField inputID;
     JTextField inputPWD;
 
-    static Font font_basic = new Font("맑은 고딕", Font.PLAIN, 12);
-    static Font font_basic_bold = new Font("맑은 고딕", Font.BOLD, 12);
-    static Font font_basic_bold_size_14 = new Font("맑은 고딕", Font.BOLD, 14);
+    Font font_basic = new Font("맑은 고딕", Font.PLAIN, 12);
+    Font font_basic_bold = new Font("맑은 고딕", Font.BOLD, 12);
+    Font font_basic_bold_size_14 = new Font("맑은 고딕", Font.BOLD, 14);
 
-    static Toolkit toolkit = Toolkit.getDefaultToolkit();
-    static Dimension screenSize = toolkit.getScreenSize();
+    Toolkit toolkit = Toolkit.getDefaultToolkit();
+    Dimension screenSize = toolkit.getScreenSize();
 
-    static public void register() {
+    public void register() {
+        // Firebase 애플리케이션 초기화
+        try {
+            FileInputStream serviceAccount = new FileInputStream("src/main/resources/key.json");
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+            FirebaseApp.initializeApp(options);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         register = new JFrame("회원가입");
 
         // 회원가입 창
@@ -32,7 +62,7 @@ public class Register extends JPanel implements ActionListener {
 
         Color loginBGC = new Color(225, 235, 239);
         registerPanel.setBackground(loginBGC);
-        // 로그인 창이 가장 상위 레벨에 뜨게 함
+        // 회원가입 창이 가장 상위 레벨에 뜨게 함
         register.setAlwaysOnTop(true);
 
         JLabel IDLabel = new JLabel("아이디");
@@ -41,7 +71,7 @@ public class Register extends JPanel implements ActionListener {
         IDLabel.setFont(font_basic_bold_size_14);
         registerPanel.add(IDLabel);
 
-        JTextField inputID = new JTextField();
+        inputID = new JTextField();
         inputID.setBounds(170, 50, 150, 20);
         inputID.setFont(font_basic);
         registerPanel.add(inputID);
@@ -52,19 +82,29 @@ public class Register extends JPanel implements ActionListener {
         PWDLabel.setFont(font_basic_bold_size_14);
         registerPanel.add(PWDLabel);
 
-        JTextField inputPWD = new JTextField();
+        inputPWD = new JTextField();
         inputPWD.setBounds(170, 75, 150, 20);
         inputPWD.setFont(font_basic);
         registerPanel.add(inputPWD);
 
-        JButton registerbtn = new JButton("회원가입");
+        registerbtn = new JButton("회원가입");
         registerbtn.setBounds(125, 230, 150, 40);
         registerbtn.setFont(font_basic_bold);
         registerPanel.add(registerbtn);
 
+        registerbtn.addActionListener(this);
+
         register.pack();
         register.setResizable(false);
         register.setVisible(true);
+    }
+
+    // 이메일 형식 검사
+    public static boolean isValid(String email) {
+        String emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 
     @Override
@@ -72,18 +112,44 @@ public class Register extends JPanel implements ActionListener {
         // 회원가입 버튼이 클릭된 경우
         if (e.getSource() == registerbtn) {
             String id = inputID.getText();
-            String pw = inputPWD.getText();
+            String pwd = inputPWD.getText();
 
-            // 회원가입에 성공한 경우
-            if (id.equals("admin") && pw.equals("1234")) {
-                JOptionPane.showMessageDialog(null, "회원가입에 성공하였습니다.", "알림", JOptionPane.INFORMATION_MESSAGE);
-                // 로그인 성공 시 게임 화면으로 전환하는 코드 추가
-                return;
+            // 빈칸으로 제출 시
+            if (id.isEmpty() || pwd.isEmpty()) {
+                JOptionPane.showMessageDialog(register, "내용을 입력하세요.", "경고", JOptionPane.WARNING_MESSAGE);
             }
 
-            // 회원가입에 실패한 경우
-            JOptionPane.showMessageDialog(null, "정보를 올바르게 입력하세요.", "알림", JOptionPane.ERROR_MESSAGE);
-            return;
+            // 이메일 형식이 아닌 경우
+            else if (!Register.isValid(id)) {
+                JOptionPane.showMessageDialog(register, "이메일 형식이 올바르지 않습니다.", "경고", JOptionPane.WARNING_MESSAGE);
+            }
+            else{
+                try {
+                    UserRecord.CreateRequest request = new UserRecord.CreateRequest()
+                            .setEmail(inputID.getText())
+                            .setPassword(inputPWD.getText());
+                    UserRecord userRecord = FirebaseAuth.getInstance().createUser(request);
+
+                    HashMap<String, Object> userData = new HashMap<>();
+                    String email = inputID.getText();
+                    String password = inputPWD.getText();
+//                    User user = new User(email, password);
+//                    userData.put("email", email);
+//                    userData.put("password", password);
+
+//                    UserData = String.valueOf(userData);
+
+                    // Realtime Database에 데이터 저장
+//                    FirebaseDatabase.getInstance().getReference("users").child(userRecord.getUid()).setValue(user);
+
+
+
+                    JOptionPane.showMessageDialog(register, "가입이 완료되었습니다.", "알림", JOptionPane.INFORMATION_MESSAGE);
+                } catch (FirebaseAuthException ex) {
+                    JOptionPane.showMessageDialog(register, "가입 중 오류가 발생하였습니다.", "오류", JOptionPane.ERROR_MESSAGE);
+                    ex.printStackTrace();
+                }
+            }
         }
     }
 }
